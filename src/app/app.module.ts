610fdddd16b561300a3d63d0cdfb3afccc754155
Tanisha Bisht <agamyatani@gmail.com>
2021-04-19 22:17:38 +0530

    routing and navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SucessComponent } from './sucess/sucess.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignUpComponent,
+    SucessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path:'', component: LoginComponent},
+      {path:'signup', component: SignUpComponent},
+      {path:'sucess', component: SucessComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
